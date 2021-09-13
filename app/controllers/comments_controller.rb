b19088eac22e8f8216157b705dd247d0eab8517b
bambi teague <teeegsa@gmail.com>
2021-09-13 09:08:59 -0500

    basic controller actions to allow us to start frontend setup

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,7 @@
 class CommentsController < ApplicationController
+  
+  def index
+    comments = Comment.all 
+    render json: comments.to_json(:include => :author)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,8 @@
 class PostsController < ApplicationController
+
+  def index
+    posts = Post.all
+    render json: posts.to_json(:include => :author)
+  end
+
 end
