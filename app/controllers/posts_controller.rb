bcc4dc02545db7ebbf80329efb3cf2744eec1b78
bambi teague <teeegsa@gmail.com>
2021-09-18 04:06:56 -0500

    filling out the Post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,43 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:destroy, :update, :show]
 
   def index
     posts = Post.all
-    render json: posts.to_json(:include => :author)
+    # byebug
+    render json: posts.to_json(:include => :author, :comments)
+   end
+
+  def show
+    render json: post
   end
+  
+  def create
+    post = Post.new(post_params)
+
+    if post.save
+      render json: post, status: :created, location: post
+    else
+      render json: post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    render json: post
+  end
+
+  def destroy
+    set_post.destroy
+    render json: {message: "successful"}
+  end
+
+  private
+   
+    def set_post
+      post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 
 end
