6f80ad46fc7b8e447d3537b990a859c9d5af8cc3
bambi teague <teeegsa@gmail.com>
2021-09-20 23:11:55 -0500

    adding validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :posts
+    
     has_secure_password
+
+    validates :username, presence: true, uniqueness: {message: "already in use"}
+    validates :password, presence: {message: "is required"}
 end
